0a67cc8c4b1d51b361238883aa20f716708ba74a
Emi Nietfeld <nietfeld@college.harvard.edu>
2013-01-19 16:03:43 -0500

    we have a store but something is broken- booo

---
+++ ELKQuestionBank.m
@@ -0,0 +1,13 @@
+//
+//  ELKQuestionBank.m
+//  emotr
+//
+//  Created by Margaret Nietfeld on 1/19/13.
+//  Copyright (c) 2013 Margaret Nietfeld. All rights reserved.
+//
+
+#import "ELKQuestionBank.h"
+
+@implementation ELKQuestionBank
+
+@end
